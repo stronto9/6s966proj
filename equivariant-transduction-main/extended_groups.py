Hopefully less hacky fix for numerical instability

equivariant-transduction-main/extended_groups.py
@@ -112,7 +112,7 @@ class SymmetricGroup(PermutationSubgroup):
         return self.matrices[i].to(self.device)
     
     def get_inverse(self, i):
-        return torch.pinverse(self.get_element(i))
+        return self.get_element(i).transpose(0,1)
 
 
 class AlternatingGroup(PermutationSubgroup):
@@ -138,7 +138,7 @@ class AlternatingGroup(PermutationSubgroup):
         return self.matrices[i].to(self.device)
     
     def get_inverse(self, i):
-        return torch.pinverse(self.get_element(i))
+        return self.get_element(i).transpose(0,1)
 
 class DihedralGroup(PermutationSubgroup):
 

equivariant-transduction-main/vocab.py
@@ -64,7 +64,7 @@ class EquivariantVocab(object):
         return self.word2idx[word]
     
     def batch_tensor_to_sent(self, ten):
-        idx_arr = torch.nonzero(torch.abs(ten.squeeze(2)) >= 1e-6)
+        idx_arr = torch.nonzero(ten.squeeze(2))
         sent = idx_arr[:, 2]
         sent = sent.view(ten.shape[0], ten.shape[1])
         return sent